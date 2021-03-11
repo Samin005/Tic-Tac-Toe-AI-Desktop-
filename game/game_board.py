669e084fbbbb49ba7d4d2096073a33cc447d3570
Samin Azhan <34473594+Samin005@users.noreply.github.com>
2021-03-11 18:19:49 +0600

    Implemented minimax using just a python list instead of numpy 2D array, as a result it's much faster now.

--- game/game_board.py
+++ game/game_board.py
@@ -3,22 +3,43 @@ import numpy as np
 board_size = 3
 board = np.zeros((3, 3), dtype=str)
 board_shape = board.shape
+players = ["X", "O"]
 
 
 def print_board_console():
     print(board)
+    # board_length = len(board)
+    # for x in range(board_length):
+    #     if (x + 1) % board_size == 0:
+    #         print(board[x], end="\n")
+    #     else:
+    #         print(board[x], end=", ")
+
+
+def make_move(player: str, board_index: int):
+    board[board_index] = player
 
 
 def check_results():
-    for x, y in np.ndindex(board_shape):
-        # horizontal match
+    winner = ""
+    for x, y in np.ndindex(board):
         if board[x, 0] == board[x, 1] and board[x, 1] == board[x, 2] and board[x, 0] != "":
-            return board[x, 0]
-        # Vertical match
-        if board[0, y] == board[x, 1] and board[1, y] == board[2, y] and board[0, y] != "":
-            return board[0, y]
-    # Diagonal match
-    if np.all(board.diagonal() == board[0, 0]) and board[0, 0] != "":
-        return board[0, 0]
-    if np.all(np.fliplr(board).diagonal() == board[0, 2]) and board[0, 2] != "":
-        return board[0, 2]
+            return
+    # board_length = len(board)
+    # horizontal_count = 0
+    # horizontal_current_player = ""
+    # for x in range(board_length):
+    #     print("x: ", x)
+    #     # setting the current player
+    #     if x % board_size == 0 and board[x] != " ":
+    #         horizontal_current_player = board[x]
+    #     # checking horizontal matches
+    #     if board[x] == horizontal_current_player:
+    #         horizontal_count += 1
+    #     if (x + 1) % board_size == 0:
+    #         if horizontal_count == board_size and board[x] == horizontal_current_player:
+    #             winner = board[x]
+    #             return winner
+    #         else:
+    #             horizontal_count = 0
+    return winner
--- game/minimax_using_numpy.py
+++ game/minimax_using_numpy.py
@@ -1,8 +1,10 @@
+# Uses a numpy 2D array and much slower
+import numpy as np
 import math
 
 board_size = 3
-board = [''] * board_size * board_size
-board_length = len(board)
+board = np.zeros((3, 3), dtype=str)
+board_shape = board.shape
 players = ["X", "O"]
 human = players[0]
 ai = players[1]
@@ -10,15 +12,11 @@ count = 0
 
 
 def print_board_console():
-    for x in range(board_length):
-        if (x + 1) % board_size == 0:
-            print(board[x], end="\n")
-        else:
-            print(board[x], end=", ")
+    print(board)
 
 
-def make_move_human(index: int):
-    board[index] = human
+def make_move_human(x: int, y: int):
+    board[x, y] = human
     print_board_console()
     winner = check_winner()
     if winner == "":
@@ -32,21 +30,21 @@ def make_move_human(index: int):
 
 def make_best_move_ai():
     best_score = -math.inf
-    best_move_index = 0
-    for index in range(board_length):
-        if board[index] == "":
-            board[index] = ai
+    best_move = {'x': 0, 'y': 0}
+    for x, y in np.ndindex(board_shape):
+        if board[x, y] == "":
+            board[x, y] = ai
             score = alpha_beta_pruning(current_board=board, level=0,
                                        alpha=-math.inf, beta=math.inf,
                                        maximizing_player=False)
-            board[index] = ""
+            board[x, y] = ""
             # print("score", score)
             if score > best_score:
                 best_score = score
-                best_move_index = index
+                best_move = {'x': x, 'y': y}
                 # print('updated best score:', best_score)
                 # print('best move:', best_move)
-    board[best_move_index] = ai
+    board[best_move['x'], best_move['y']] = ai
     print_board_console()
     winner = check_winner()
     if winner == "tie":
@@ -55,48 +53,34 @@ def make_best_move_ai():
         print(winner, "has won the game!")
 
 
-def find_match(index_1: int, index_2: int, index_3: int):
-    if board[index_1] == board[index_2] and board[index_2] == board[index_3] and board[index_1] != "":
-        return board[index_1]
-
-
 def check_winner():
     # global count
     # print(count)
     # count += 1
     winner = ""
-    # Horizontal match
-    result = find_match(0, 1, 2)
-    if result:
-        winner = result
-    result = find_match(3, 4, 5)
-    if result:
-        winner = result
-    result = find_match(6, 7, 8)
-    if result:
-        winner = result
-
-    # Vertical match
-    result = find_match(0, 3, 6)
-    if result:
-        winner = result
-    result = find_match(1, 4, 7)
-    if result:
-        winner = result
-    result = find_match(2, 5, 8)
-    if result:
-        winner = result
-
+    for x, y in np.ndindex(board_shape):
+        # Horizontal match
+        if board[x, 0] == board[x, 1] and board[x, 1] == board[x, 2] and board[x, 0] != "":
+            # print(board)
+            # print("horizontal match:", x, 0)
+            winner = board[x, 0]
+        # Vertical match
+        if board[0, y] == board[1, y] and board[1, y] == board[2, y] and board[0, y] != "":
+            # print(board)
+            # print("vertical match:", 0, y)
+            winner = board[0, y]
     # Diagonal match
-    result = find_match(0, 4, 8)
-    if result:
-        winner = result
-    result = find_match(2, 4, 6)
-    if result:
-        winner = result
+    if np.all(board.diagonal() == board[0, 0]) and board[0, 0] != "":
+        # print(board)
+        # print("left diagonal match:", 0, 0)
+        winner = board[0, 0]
+    if np.all(np.fliplr(board).diagonal() == board[0, 2]) and board[0, 2] != "":
+        # print(board)
+        # print("left diagonal match:", 0, 2)
+        winner = board[0, 2]
     open_spots = 0
-    for index in range(board_length):
-        if board[index] == "":
+    for x, y in np.ndindex(board_shape):
+        if board[x, y] == "":
             open_spots += 1
     if winner == "" and open_spots == 0:
         # print("returning tie")
@@ -119,7 +103,7 @@ def get_scores(player: str):
         return 0
 
 
-def alpha_beta_pruning(current_board: list, level: int, alpha, beta, maximizing_player: bool):
+def alpha_beta_pruning(current_board: np.ndarray, level: int, alpha, beta, maximizing_player: bool):
     global count
     count += 1
     winner = check_winner()  # returns X or O or ""
@@ -129,15 +113,15 @@ def alpha_beta_pruning(current_board: list, level: int, alpha, beta, maximizing_
         return get_scores(winner)  # returning the game value
     if maximizing_player:
         max_score = -math.inf
-        for index in range(board_length):
-            if current_board[index] == "":
-                current_board[index] = ai
+        for x, y in np.ndindex(board_shape):
+            if current_board[x, y] == "":
+                current_board[x, y] = ai
                 score = alpha_beta_pruning(current_board=current_board, level=level + 1,
                                            alpha=alpha, beta=beta,
                                            maximizing_player=False)
                 # print(board)
                 # print("current score", score)
-                current_board[index] = ""  # taking the move back because we want to find the best move first
+                current_board[x, y] = ""  # taking the move back because we want to find the best move first
                 max_score = max(max_score, score)
                 # print("max score", max_score)
                 alpha = max(alpha, score)
@@ -146,15 +130,15 @@ def alpha_beta_pruning(current_board: list, level: int, alpha, beta, maximizing_
         return max_score
     else:
         min_score = math.inf
-        for index in range(board_length):
-            if current_board[index] == "":
-                current_board[index] = human
+        for x, y in np.ndindex(board_shape):
+            if current_board[x, y] == "":
+                current_board[x, y] = human
                 score = alpha_beta_pruning(current_board=current_board, level=level + 1,
                                            alpha=alpha, beta=beta,
                                            maximizing_player=True)
                 # print(board)
                 # print("current score", score)
-                current_board[index] = ""  # taking the move back because we want to find the best move first
+                current_board[x, y] = ""  # taking the move back because we want to find the best move first
                 min_score = min(min_score, score)
                 # print("min score", min_score)
                 beta = min(beta, score)
